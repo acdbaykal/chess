e00c4f8b4d6977888e8d8086a5f25c47021dc740
Ali Baykal <alib@dasburo.com>
2022-03-11 21:43:40 +0100

    test: Reorder rook rules test code

--- src/domain/rules/move/__tests__/rook.test.ts
+++ src/domain/rules/move/__tests__/rook.test.ts
@@ -19,130 +19,131 @@ describe('domain/rules/moves/rook', () => {
                 expect(result).toEqual([]);
             })
         });
-    });
 
-    describe('board with single piece', () => {
-        it('returns empty array when the piece at given squatre is not a rook', () => {
-            const moveStart = createSquare(D, _5);
-            const board = createBoardFromList([
-                [moveStart, createPiece(PieceColor.Black, PieceType.Knight)]
-            ]);
-            const result = getLegalMoves(board, moveStart);
-            expect(result).toEqual([]);
+        describe('board with single piece', () => {
+            it('returns empty array when the piece at given squatre is not a rook', () => {
+                const moveStart = createSquare(D, _5);
+                const board = createBoardFromList([
+                    [moveStart, createPiece(PieceColor.Black, PieceType.Knight)]
+                ]);
+                const result = getLegalMoves(board, moveStart);
+                expect(result).toEqual([]);
+            });
+    
+    
+            it('returns all legal moves until the edeg of the board', () => {
+                const test  = (square: Square, expected: Square[]) =>{
+                    const board = createBoardFromList([[square, createPiece(PieceColor.White, PieceType.Rook)]]);
+                    
+                    const moves = pipe(
+                        getLegalMoves(board, square),
+                        sortMoveList
+                    );
+                    const expectedMoves = pipe(
+                        expected,
+                        createMoveList(square),
+                        sortMoveList
+                    );
+                    expect(moves).toEqual(expectedMoves);
+                };
+    
+                test(createSquare(D, _5), [
+                    createSquare(D, _1),
+                    createSquare(D, _2),
+                    createSquare(D, _3),
+                    createSquare(D, _4),
+                    createSquare(D, _6),
+                    createSquare(D, _7),  
+                    createSquare(D, _8),
+                    createSquare(A, _5),
+                    createSquare(B, _5),
+                    createSquare(C, _5),
+                    createSquare(E, _5),
+                    createSquare(F, _5),
+                    createSquare(G, _5),
+                    createSquare(H, _5),
+                ]);
+            });
         });
-
-
-        it('returns all legal moves until the edeg of the board', () => {
-            const test  = (square: Square, expected: Square[]) =>{
-                const board = createBoardFromList([[square, createPiece(PieceColor.White, PieceType.Rook)]]);
+    
+        describe('with pieces of the same color on the board', () => {
+            const test  = (color: PieceColor) =>{
+                const square = createSquare(D, _5);
+                const board = createBoardFromList([
+                    [square, createPiece(color, PieceType.Rook)],
+                    [createSquare(D, _7), createPiece(color, PieceType.Pawn)],
+                    [createSquare(D, _1), createPiece(color, PieceType.Knight)],
+                    [createSquare(D, _3), createPiece(color, PieceType.Bishop)],
+                    [createSquare(D, _4), createPiece(color, PieceType.Queen)],
+                    [createSquare(B, _5), createPiece(color, PieceType.Pawn)],
+                    [createSquare(G, _5), createPiece(color, PieceType.Pawn)]
+                ]);
                 
                 const moves = pipe(
                     getLegalMoves(board, square),
                     sortMoveList
                 );
                 const expectedMoves = pipe(
-                    expected,
+                    [
+                        createSquare(D, _6),
+                        createSquare(C, _5),
+                        createSquare(E, _5),
+                        createSquare(F, _5),
+                    ],
                     createMoveList(square),
                     sortMoveList
                 );
                 expect(moves).toEqual(expectedMoves);
             };
-
-            test(createSquare(D, _5), [
-                createSquare(D, _1),
-                createSquare(D, _2),
-                createSquare(D, _3),
-                createSquare(D, _4),
-                createSquare(D, _6),
-                createSquare(D, _7),  
-                createSquare(D, _8),
-                createSquare(A, _5),
-                createSquare(B, _5),
-                createSquare(C, _5),
-                createSquare(E, _5),
-                createSquare(F, _5),
-                createSquare(G, _5),
-                createSquare(H, _5),
-            ]);
+    
+            it('stops at pice with the same color', () => {
+                test(PieceColor.White);
+                test(PieceColor.Black);
+            });
+            
         });
-    });
-
-    describe('with pieces of the same color on the board', () => {
-        const test  = (color: PieceColor) =>{
-            const square = createSquare(D, _5);
-            const board = createBoardFromList([
-                [square, createPiece(color, PieceType.Rook)],
-                [createSquare(D, _7), createPiece(color, PieceType.Pawn)],
-                [createSquare(D, _1), createPiece(color, PieceType.Knight)],
-                [createSquare(D, _3), createPiece(color, PieceType.Bishop)],
-                [createSquare(D, _4), createPiece(color, PieceType.Queen)],
-                [createSquare(B, _5), createPiece(color, PieceType.Pawn)],
-                [createSquare(G, _5), createPiece(color, PieceType.Pawn)]
-            ]);
+    
+        describe('with pieces of the opposite color on the board', () => {
             
-            const moves = pipe(
-                getLegalMoves(board, square),
-                sortMoveList
-            );
-            const expectedMoves = pipe(
-                [
-                    createSquare(D, _6),
-                    createSquare(C, _5),
-                    createSquare(E, _5),
-                    createSquare(F, _5),
-                ],
-                createMoveList(square),
-                sortMoveList
-            );
-            expect(moves).toEqual(expectedMoves);
-        };
+            it('includes the pices in the move destionations and stops', () => {
+                const test  = (color: PieceColor) => {
+                    const square = createSquare(D, _5);
+                    const oppositeColor = reversePieceColor(color);
+                    const board = createBoardFromList([
+                        [square, createPiece(color, PieceType.Rook)],
+                        [createSquare(D, _7), createPiece(oppositeColor, PieceType.Pawn)],
+                        [createSquare(D, _1), createPiece(oppositeColor, PieceType.Knight)],
+                        [createSquare(D, _3), createPiece(oppositeColor, PieceType.Bishop)],
+                        [createSquare(D, _4), createPiece(oppositeColor, PieceType.Queen)],
+                        [createSquare(B, _5), createPiece(oppositeColor, PieceType.Pawn)],
+                        [createSquare(G, _5), createPiece(oppositeColor, PieceType.Pawn)]
+                    ]);
+                    
+                    const moves = pipe(
+                        getLegalMoves(board, square),
+                        sortMoveList
+                    );
+                    
+                    const expectedMoves = pipe(
+                        [
+                            createSquare(D, _6),
+                            createSquare(D, _7),
+                            createSquare(D, _4),
+                            createSquare(C, _5),
+                            createSquare(B, _5),
+                            createSquare(E, _5),
+                            createSquare(F, _5),
+                            createSquare(G, _5),
+                        ],
+                        createMoveList(square),
+                        sortMoveList
+                    );
+                    expect(moves).toEqual(expectedMoves);
+                };
 
-        it('stops at pice with the same color', () => {
-            test(PieceColor.White);
-            test(PieceColor.Black);
-        });
-        
-    });
-
-    describe('with pieces of the opposite color on the board', () => {
-        const test  = (color: PieceColor) => {
-            const square = createSquare(D, _5);
-            const oppositeColor = reversePieceColor(color);
-            const board = createBoardFromList([
-                [square, createPiece(color, PieceType.Rook)],
-                [createSquare(D, _7), createPiece(oppositeColor, PieceType.Pawn)],
-                [createSquare(D, _1), createPiece(oppositeColor, PieceType.Knight)],
-                [createSquare(D, _3), createPiece(oppositeColor, PieceType.Bishop)],
-                [createSquare(D, _4), createPiece(oppositeColor, PieceType.Queen)],
-                [createSquare(B, _5), createPiece(oppositeColor, PieceType.Pawn)],
-                [createSquare(G, _5), createPiece(oppositeColor, PieceType.Pawn)]
-            ]);
-            
-            const moves = pipe(
-                getLegalMoves(board, square),
-                sortMoveList
-            );
-            
-            const expectedMoves = pipe(
-                [
-                    createSquare(D, _6),
-                    createSquare(D, _7),
-                    createSquare(D, _4),
-                    createSquare(C, _5),
-                    createSquare(B, _5),
-                    createSquare(E, _5),
-                    createSquare(F, _5),
-                    createSquare(G, _5),
-                ],
-                createMoveList(square),
-                sortMoveList
-            );
-            expect(moves).toEqual(expectedMoves);
-        };
-
-        it('includes the pices in the move destionations and stops', () => {
-            test(PieceColor.White);
-            test(PieceColor.Black);
+                test(PieceColor.White);
+                test(PieceColor.Black);
+            });
         });
     });
 });

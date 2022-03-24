9a30693e3ac49a4d766fbcdbce0d403265c87263
Ali Baykal <alib@dasburo.com>
2022-03-24 21:18:30 +0100

    Calculate double forward moves for pawns

--- src/domain/rules/move/__tests__/pawn.test.ts
+++ src/domain/rules/move/__tests__/pawn.test.ts
@@ -1,3 +1,4 @@
+import { pipe } from 'fp-ts/lib/pipeable';
 import { createBoardFromList } from '../../../entities/board/constructors';
 import { createGame } from '../../../entities/game/constructors';
 import { createMove } from '../../../entities/move/constructors';
@@ -6,12 +7,12 @@ import { Move } from '../../../entities/move/Move';
 import { createPiece } from '../../../entities/piece/constructors';
 import { PieceColor, PieceType } from '../../../entities/piece/Piece';
 import { createSquare } from '../../../entities/square/constructors';
-import { B, E, Square, _2, _3, _4 } from '../../../entities/square/Square';
+import { B, E, Square, _2, _3, _4, _5, _6, _7 } from '../../../entities/square/Square';
 import {getLegalMoves} from '../pawn';
 
 describe('domain/rule/move/pawn', () => {
     describe("moving a single square ahead", () => {
-        test('it moves includes moving ahead if the squre is not accupied', () => {
+        it('includes moving ahead if the squre is not accupied', () => {
             const test = (start:Square, pieceColor: PieceColor, end: Square) => {
                 const board = createBoardFromList([
                     [start, createPiece(pieceColor, PieceType.Pawn)]
@@ -24,7 +25,7 @@ describe('domain/rule/move/pawn', () => {
                     end
                 );
                 const forwardMove = legalMoves.find(isSameMoveAs(expected));
-                expect(forwardMove).toEqual(expected)
+                expect(forwardMove).toEqual(expected);
             };
 
             test(createSquare(E, _3), PieceColor.Black, createSquare(E, _2));
@@ -34,14 +35,81 @@ describe('domain/rule/move/pawn', () => {
 
         it('does not include forward move if the square is occupied', () => {
             const square = createSquare(B, _3);
+            const destination = createSquare(B, _2);
             const board = createBoardFromList([
                 [square, createPiece(PieceColor.Black, PieceType.Pawn)],
-                [createSquare(B, _2), createPiece(PieceColor.Black, PieceType.Knight)]
+                [destination, createPiece(PieceColor.Black, PieceType.Knight)]
             ]);
 
             const game = createGame(board, []);
             const legalMoves:Move[] = getLegalMoves(game, square);
-            expect(legalMoves).toHaveLength(0);
+            const nonlegal = createMove(square, destination);
+            const forwardMove = legalMoves.find(isSameMoveAs(nonlegal));
+            expect(forwardMove).not.toBeDefined();
+        });
+    });
+
+    describe('moving two squares ahead', () => {
+        it('includes moving two squeares ahead, when the pawn is at it\'s home row', () => {
+            const test = (pieceColor: PieceColor) => {
+                const row = pieceColor === PieceColor.Black ? _7 : _2;
+                const destibationRow = pieceColor === PieceColor.Black ? _5 : _4;
+                const start = createSquare(B, row);
+                const board = createBoardFromList([
+                    [start, createPiece(pieceColor, PieceType.Pawn)]
+                ]);
+                const game = createGame(board, []);
+                const destination = createSquare(B, destibationRow);
+                const legalMoves:Move[] = getLegalMoves(game, start);
+                const expected = createMove(start, destination);
+                const forwardMove = legalMoves.find(isSameMoveAs(expected));
+                expect(forwardMove).toEqual(expected)
+            };
+
+            test(PieceColor.Black);
+            test(PieceColor.White);
+        });
+
+        it('excludes moving two squeares ahead, when the pawn is NOT at it\'s home row', () => {
+            const test = (pieceColor: PieceColor) => {
+                const row = _4;
+                const destibationRow = pieceColor === PieceColor.Black ? _2 : _6;
+                const start = createSquare(B, row);
+                const board = createBoardFromList([
+                    [start, createPiece(pieceColor, PieceType.Pawn)]
+                ]);
+                const game = createGame(board, []);
+                const destination = createSquare(B, destibationRow);
+                const legalMoves:Move[] = getLegalMoves(game, start);
+                const nonlegal = createMove(start, destination);
+                const forwardMove = legalMoves.find(isSameMoveAs(nonlegal));
+                expect(forwardMove).not.toBeDefined();
+            };
+
+            test(PieceColor.Black);
+            test(PieceColor.White);
+        });
+
+        it('exludes moving two squeares ahead, when the destination is occupied', () => {
+            const test = (pieceColor: PieceColor) => {
+                const row = pieceColor === PieceColor.Black ? _7 : _2;
+                const destibationRow = pieceColor === PieceColor.Black ? _5 : _4;
+                const destination = createSquare(B, destibationRow);
+                const start = createSquare(B, row);
+                const board = createBoardFromList([
+                    [start, createPiece(pieceColor, PieceType.Pawn)],
+                    [destination, createPiece(PieceColor.White, PieceType.Bishop)]
+                ]);
+                const game = createGame(board, []);
+
+                const legalMoves:Move[] = getLegalMoves(game, start);
+                const nonlegal = createMove(start, destination);
+                const forwardMove = legalMoves.find(isSameMoveAs(nonlegal));
+                expect(forwardMove).not.toBeDefined();
+            };
+
+            test(PieceColor.Black);
+            test(PieceColor.White);
         });
     });
 });
--- src/domain/rules/move/pawn.ts
+++ src/domain/rules/move/pawn.ts
@@ -1,39 +1,68 @@
 import { Game } from "../../entities/game/Game";
 import { Move } from "../../entities/move/Move";
-import { toString } from "../../entities/square/getters";
-import { Square } from "../../entities/square/Square";
+import { getNumericAxis, toString } from "../../entities/square/getters";
+import { Square, _2, _7 } from "../../entities/square/Square";
 import {toLower, toUpper} from '../../entities/square/transitions';
-import { chain as chainEither, filterOrElse, fold, fromOption } from 'fp-ts/Either';
+import { chain as chainEither, map as mapRight, filterOrElse, fold, fromOption, getOrElse, Either } from 'fp-ts/Either';
 import { createMove } from "../../entities/move/constructors";
 import { PieceColor } from "../../entities/piece/Piece";
 import { getCurrentBoard } from "../../entities/game/getters";
 import { getPieceColorAt, isSquareOccupied } from "../../entities/board/getters";
-import { pipe } from "fp-ts/function";
-import { map as mapOption} from "fp-ts/lib/Option";
+import { flow, pipe } from "fp-ts/function";
+import { map as mapOption, filter as filterOption} from "fp-ts/lib/Option";
 import { Board } from "../../entities/board/Board";
+import { flatten, map } from "fp-ts/lib/Array";
+
+const getForwardSquare = (amount:number) => (pieceColor: PieceColor) =>
+    pieceColor === PieceColor.Black ? toLower(amount) : toUpper(amount);
 
 
-const getForwardSquare = (pieceColor: PieceColor) => 
-    pieceColor === PieceColor.Black ? toLower(1) : toUpper(1);
+const getSingleForwardSquare = getForwardSquare(1);
+const getDoubleForwardSqure = getForwardSquare(2);
 
-const getForwardSquareFromBoard = (square: Square) => (board:Board) => {
-    console.log(board);
-    
-    return pipe(
-    getPieceColorAt(board, square),
-    mapOption(getForwardSquare),
-    fromOption(() => Error(`${toString(square)} is not oocupied by a pawn`)),
-    chainEither(_getForwardSquare => _getForwardSquare(square)),
-    filterOrElse(sq => !isSquareOccupied(board, sq), () => Error('Sqaure is already occupied'))
-)}
+const isAtHome = (square: Square) => (pieceColor: PieceColor): boolean =>
+    (pieceColor === PieceColor.Black && getNumericAxis(square) === _7) ||
+    (pieceColor === PieceColor.White && getNumericAxis(square) === _2)
+
+const getForwardSingleSquareFromBoard = (square: Square) => (board:Board) =>
+    pipe(
+        getPieceColorAt(board, square),
+        mapOption(getSingleForwardSquare),
+        fromOption(() => Error(`${toString(square)} is not oocupied by a pawn`)),
+        chainEither(_getForwardSquare => _getForwardSquare(square)),
+        filterOrElse(sq => !isSquareOccupied(board, sq), () => Error('Sqaure is already occupied'))
+    );
+
+const getForwardDoubleSquareFromBoard = (square: Square) => (board:Board) =>
+    pipe(
+        getPieceColorAt(board, square),
+        filterOption(isAtHome(square)),
+        mapOption(getDoubleForwardSqure),
+        fromOption(() => Error(`${toString(square)} is not oocupied by a pawn or is not home row`)),
+        chainEither(_getForwardSquare => _getForwardSquare(square)),
+        filterOrElse(sq => !isSquareOccupied(board, sq), () => Error('Sqaure is already occupied'))
+    );
+
+
+const getOrEmpty = (square:Square) => fold<Error, Square, Move[]>(
+    () => [],
+    destination => [createMove(square, destination)]
+);
+
+const combineMoves = (square:Square) => flow(
+    (board: Board): Either<Error, Square>[] => [
+        getForwardSingleSquareFromBoard(square)(board),
+        getForwardDoubleSquareFromBoard(square)(board)
+    ],
+    map(getOrEmpty(square)),
+    flatten
+)
 
 export const getLegalMoves = (game: Game, square:Square): Move[] => {
     return pipe(
         getCurrentBoard(game),
-        chainEither(getForwardSquareFromBoard(square)),
-        fold<Error, Square, Move[]>(
-            () => [],
-            destination => [createMove(square, destination)]
-        )
+        // @ts-expect-error
+        mapRight(combineMoves(square)),
+        getOrElse(() => [])
     );
 }

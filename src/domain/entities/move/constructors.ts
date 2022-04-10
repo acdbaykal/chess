af697366e2626d61a6b50fde814e2ccf85412f29
Ali Baykal <alib@dasburo.com>
2022-04-10 12:57:58 +0200

    ref: Switch to immutable js records for move entity

--- src/domain/entities/move/constructors.ts
+++ src/domain/entities/move/constructors.ts
@@ -1,14 +1,30 @@
+import { A, Square, _1, _2 } from "../square/Square";
+import { Move, MoveType, Promotion, PromotionPieceType, RegularMove } from "./Move";
+import { Record } from "immutable";
+import { createSquare } from "../square/constructors";
 import { PieceType } from "../piece/Piece";
-import { Square } from "../square/Square";
-import { Move, MoveType, Promotion, RegularMove } from "./Move";
+
+
+const RegularMoveFactory = Record<RegularMove>({
+    from: createSquare(A, _1),
+    to: createSquare(A, _2),
+    __type__: MoveType.REGULAR
+});
+
+const PromotionMoveFactory = Record<Promotion>({
+    from: createSquare(A, _1),
+    to: createSquare(A, _2),
+    __type__: MoveType.PROMOTION,
+    pieceType: PieceType.Queen
+});
 
 export const createRegularMove = (from: Square, to: Square):RegularMove => 
-    ({ from,to, __type__: MoveType.REGULAR});
+    RegularMoveFactory({ from,to, __type__: MoveType.REGULAR});
 
-export const createPromotion = (from: Square, to: Square, pieceType: PieceType):Promotion =>
-    ({from, to, __type__: MoveType.PROMOTION, pieceType})
+export const createPromotion = (from: Square, to: Square, pieceType: PromotionPieceType):Promotion =>
+    PromotionMoveFactory({from, to, __type__: MoveType.PROMOTION, pieceType})
     
-export const createMoveList = (start: Square) => (destinations: (Square | [Square, PieceType])[]): Move[] =>
+export const createMoveList = (start: Square) => (destinations: (Square | [Square, PromotionPieceType])[]): Move[] =>
     destinations.map(
         dest => dest instanceof Array ? createPromotion(start, dest[0], dest[1]) : createRegularMove(start, dest)
     );

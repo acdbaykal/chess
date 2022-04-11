0a15b415ff786d934a1469707b2a59237a7b5a93
Ali Baykal <alib@dasburo.com>
2022-04-11 09:45:36 +0200

    Get a move from a move history

--- src/domain/entities/movehistory/__test__/getters.test.ts
+++ src/domain/entities/movehistory/__test__/getters.test.ts
@@ -1,8 +1,11 @@
-import { calcHashCode, equals } from "../getters";
+import { calcHashCode, equals, getMoveAt, getLastMove, getPreviousMove } from "../getters";
 import { createRegularMove } from "../../move/constructors";
 import { createSquare } from "../../square/constructors";
-import { E, _2, _4, _5, _7 } from "../../square/Square";
+import { B, C, E, _1, _2, _3, _4, _5, _7 } from "../../square/Square";
 import { createMoveHistory} from "../constructors";
+import { Move } from "../../move/Move";
+import { none, some } from "fp-ts/lib/Option";
+import { MoveHistory } from "../MoveHistory";
 
 describe('domain/entities/movehistory', () => {
     describe('calcHashCode', () => {
@@ -74,6 +77,118 @@ describe('domain/entities/movehistory', () => {
             ]);
 
             expect(equals(moveList1)(moveList2)).toBe(false);
+        });
+    });
+
+    describe('get move at index', () => {
+        const moveHistory = createMoveHistory([
+            createRegularMove(
+                createSquare(E, _2),
+                createSquare(E, _4)
+            ),
+            createRegularMove(
+                createSquare(E, _7),
+                createSquare(E, _5)
+            ),
+            createRegularMove(
+                createSquare(B, _1),
+                createSquare(C, _3)
+            )
+        ]);
+
+
+        it('returns the move at given index within an option', () => {
+            
+            const test = (index:number, expected: Move) => {
+                const move = getMoveAt(index)(moveHistory);
+                expect(move).toEqual(some(expected));
+            };
+
+            test(1, createRegularMove(
+                createSquare(E, _7),
+                createSquare(E, _5)
+            ));
+
+            test(2, createRegularMove(
+                createSquare(B, _1),
+                createSquare(C, _3)
+            ));
+        });
+
+        it('returns none when index is outof bounds',  ()=> {
+            const test = (moveHistory:MoveHistory, index:number) => {
+                const move = getMoveAt(index)(moveHistory);
+                expect(move).toEqual(none);
+            };
+
+            test(moveHistory, 3);
+            test(moveHistory, -1);
+            test(createMoveHistory([]), 0);
+        });
+    });
+
+    describe('get last move', () => {
+        it('returns last move within an option', () => {
+            const moveHistory = createMoveHistory([
+                createRegularMove(
+                    createSquare(E, _2),
+                    createSquare(E, _4)
+                ),
+                createRegularMove(
+                    createSquare(E, _7),
+                    createSquare(E, _5)
+                )
+            ]);
+
+            const lastMove = getLastMove(moveHistory);
+
+            expect(lastMove).toEqual(some(createRegularMove(
+                createSquare(E, _7),
+                createSquare(E, _5)
+            )));
+        });
+
+        it('returns none when empty', () => {
+            const moveHistory = createMoveHistory([]);
+            const lastMove = getLastMove(moveHistory);
+            expect(lastMove).toBe(none);
+        });
+    });
+
+    describe('getPrevious move', () => {
+        it('returns the second last move', () => {
+            const moveHistory = createMoveHistory([
+                createRegularMove(
+                    createSquare(E, _2),
+                    createSquare(E, _4)
+                ),
+                createRegularMove(
+                    createSquare(E, _7),
+                    createSquare(E, _5)
+                )
+            ]);
+
+            const previousMove = getPreviousMove(moveHistory);
+            expect(previousMove).toEqual(some(createRegularMove(
+                createSquare(E, _2),
+                createSquare(E, _4)
+            )));
+        });
+
+
+        it('returns none when empty or with single element', () => {
+            const test = (moves: Move[]) => {
+                const moveHistory = createMoveHistory(moves);
+                const lastMove = getPreviousMove(moveHistory);
+                expect(lastMove).toBe(none);
+            };
+            test([]);
+            test([
+                createRegularMove(
+                    createSquare(E, _7),
+                    createSquare(E, _5)
+                )
+            ]);
         });
     });
 });
--- src/domain/entities/movehistory/getters.ts
+++ src/domain/entities/movehistory/getters.ts
@@ -1,10 +1,28 @@
 import {MoveHistory} from './MoveHistory';
 import {List} from 'immutable';
 import { Move } from '../move/Move';
+import { none} from 'fp-ts/lib/Option';
+import { fromNullable } from 'fp-ts/lib/Option';
 
 export const calcHashCode = (movesHistory: MoveHistory):number =>
     (movesHistory as unknown as List<Move>).hashCode()
 
 export const equals = (movesHistory1: MoveHistory) => (movesHistory2: MoveHistory): boolean =>
     calcHashCode(movesHistory1) === calcHashCode(movesHistory2);
+
+
+export const getMoveAt = (index: number) => (moveHistory: MoveHistory) =>
+    index < 0
+        ? none
+        : fromNullable(moveHistory[index]);
+
+export const getLastMove = (moveHistory:MoveHistory) => {
+    const index = moveHistory.length - 1;
+    return getMoveAt(index)(moveHistory);
+};
+
+export const getPreviousMove = (moveHistory: MoveHistory) => {
+    const index = moveHistory.length - 2;
+    return getMoveAt(index)(moveHistory);
+};
     

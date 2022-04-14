e606030bbb9a1d4204782df591d783d00d635438
Ali Baykal <alib@dasburo.com>
2022-04-15 01:30:02 +0200

    Prevent kings from getting next to eachother

--- src/domain/rules/move/king.ts
+++ src/domain/rules/move/king.ts
@@ -8,21 +8,34 @@ import { filter as filterList, map as mapList } from 'fp-ts/Array'
 import { map as mapEither } from "fp-ts/lib/Either";
 import { createRegularMove } from "../../entities/move/constructors";
 import { getCurrentBoard } from "../../entities/game/getters";
-import { getPieceColorAt, isOccupiedByColor } from "../../entities/board/getters";
-import { map as mapOption } from 'fp-ts/Option';
-import { getOrFalse } from "../../../lib/option";
+import { getPieceColorAt, isOccupiedByColor, isSquareOccupiedByPiece } from "../../entities/board/getters";
+import { map as mapOption, getOrElse } from 'fp-ts/Option';
+import { PieceColor, PieceType } from "../../entities/piece/Piece";
+import { reversePieceColor } from "../../entities/piece/transition";
+import { any } from "ramda";
+import { createPiece } from "../../entities/piece/constructors";
 
-const isNotOccupiedBySameColor = (board:Board, square:Square) => (destination: Square):boolean =>
-    pipe(
-        getPieceColorAt(board, square),
-        mapOption(pieceColor => !isOccupiedByColor(pieceColor)(board, destination)),
-        getOrFalse
+const isNotOccupiedBySameColor = (board:Board, kingColor: PieceColor) => (square:Square):boolean =>
+       !isOccupiedByColor(kingColor)(board, square)
+
+const neighborsKing = (board: Board, kingColor: PieceColor) => (square: Square):boolean =>{
+    const oppositeKing = createPiece(reversePieceColor(kingColor), PieceType.King);
+
+    return pipe(
+        calcNeighbors(square),
+        any(neighbor => isSquareOccupiedByPiece(oppositeKing)(board, neighbor))
     );
- 
+};
+
 const getNonCastlingMoves = (board: Board, square: Square):Move[] => pipe(
-    calcNeighbors(square),
-    filterList(isNotOccupiedBySameColor(board, square)),
-    mapList(destination => createRegularMove(square, destination))
+    getPieceColorAt(board, square),
+    mapOption(kingColor => pipe(
+        calcNeighbors(square),
+        filterList(isNotOccupiedBySameColor(board, kingColor)),
+        filterList(neighbor => !neighborsKing(board, kingColor)(neighbor)),
+        mapList(destination => createRegularMove(square, destination))
+    )),
+    getOrElse<Move[]>(() => [])
 );
 
 export const getLegalMoves = (game: Game, square: Square) =>  pipe(

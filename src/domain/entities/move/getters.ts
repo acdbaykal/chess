ba4f25846d38e2764896a90917dbf2981986052b
Ali Baykal <alib@dasburo.com>
2022-04-23 12:49:58 +0200

    Introduce en passant as a special move type

--- src/domain/entities/move/getters.ts
+++ src/domain/entities/move/getters.ts
@@ -1,8 +1,6 @@
 import { flow } from "fp-ts/lib/function";
-import { is } from "ramda";
-import { PieceType } from "../piece/Piece";
 import { getRank, squareEquals } from "../square/getters";
-import { Move, MoveType, Promotion, PromotionPieceType, RegularMove } from "./Move";
+import { Move, MoveType, EnPassant, Promotion, PromotionPieceType, RegularMove } from "./Move";
 
 export const getMoveFrom = (move: Move) => move.from;
 export const getMoveTo = (move: Move) => move.to;
@@ -10,11 +8,15 @@ export const isSameMoveAs = (move1:Move) => (move2:Move):boolean =>
     squareEquals(getMoveFrom(move1))(getMoveFrom(move2)) && 
     squareEquals(getMoveTo(move1))(getMoveTo(move2));
 
-export const isRegularMove = (move: Move):move is RegularMove =>
-    move.__type__ === MoveType.REGULAR;
+const isOfType = <M extends Move>(moveType: MoveType) => (move:Move): move is M =>
+    move.__type__ === moveType
 
-export const isPromotionMove = (move: Move):move is Promotion =>
-    move.__type__ === MoveType.PROMOTION;
+
+export const isRegularMove = isOfType<RegularMove>(MoveType.REGULAR);
+
+export const isPromotionMove = isOfType<Promotion>(MoveType.PROMOTION);
+
+export const isEnPassant = isOfType<EnPassant>(MoveType.ENPASSANT);
 
 export const getPromotionPieceType = (move: Promotion): PromotionPieceType =>
     move.pieceType;

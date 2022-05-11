32798333af1813c26dfb787f088ec4d965f090de
Ali Baykal <alib@dasburo.com>
2022-05-11 22:12:35 +0200

    Move boardToString into conversions file

---
+++ src/domain/entities/board/conversions.ts
@@ -0,0 +1,106 @@
+import { flow } from "fp-ts/lib/function"
+import { getOrElse, map as mapOption } from "fp-ts/lib/Option"
+import { pieceToEmoji } from "../piece/transition"
+import { createSquare } from "../square/constructors"
+import { _1, _2, _3, _4, _5, _6, _7, _8, A, B, C, D, E, F, G, H, Square } from "../square/Square"
+import { Board } from "./Board"
+import { getPieceAt } from "./getters"
+
+const RENDER_ORDER: Square[][] = [
+    [
+        createSquare(A, _8),
+        createSquare(B, _8),
+        createSquare(C, _8),
+        createSquare(D, _8),
+        createSquare(E, _8),
+        createSquare(F, _8),
+        createSquare(G, _8),
+        createSquare(H, _8),
+    ],
+    [
+        createSquare(A, _7),
+        createSquare(B, _7),
+        createSquare(C, _7),
+        createSquare(D, _7),
+        createSquare(E, _7),
+        createSquare(F, _7),
+        createSquare(G, _7),
+        createSquare(H, _7),
+    ],
+    [
+        createSquare(A, _6),
+        createSquare(B, _6),
+        createSquare(C, _6),
+        createSquare(D, _6),
+        createSquare(E, _6),
+        createSquare(F, _6),
+        createSquare(G, _6),
+        createSquare(H, _6),
+    ],
+    [
+        createSquare(A, _5),
+        createSquare(B, _5),
+        createSquare(C, _5),
+        createSquare(D, _5),
+        createSquare(E, _5),
+        createSquare(F, _5),
+        createSquare(G, _5),
+        createSquare(H, _5),
+    ],
+    [
+        createSquare(A, _4),
+        createSquare(B, _4),
+        createSquare(C, _4),
+        createSquare(D, _4),
+        createSquare(E, _4),
+        createSquare(F, _4),
+        createSquare(G, _4),
+        createSquare(H, _4),
+    ],
+    [
+        createSquare(A, _3),
+        createSquare(B, _3),
+        createSquare(C, _3),
+        createSquare(D, _3),
+        createSquare(E, _3),
+        createSquare(F, _3),
+        createSquare(G, _3),
+        createSquare(H, _3),
+    ],
+    [
+        createSquare(A, _2),
+        createSquare(B, _2),
+        createSquare(C, _2),
+        createSquare(D, _2),
+        createSquare(E, _2),
+        createSquare(F, _2),
+        createSquare(G, _2),
+        createSquare(H, _2),
+    ],
+    [
+        createSquare(A, _1),
+        createSquare(B, _1),
+        createSquare(C, _1),
+        createSquare(D, _1),
+        createSquare(E, _1),
+        createSquare(F, _1),
+        createSquare(G, _1),
+        createSquare(H, _1),
+    ]
+]
+
+export const boardToString  = (board:Board  ):string => 
+    ['A B C D E F G H'].concat(
+        RENDER_ORDER.map(
+            line => 
+                line.map(
+                    flow(
+                        sqr => getPieceAt(board, sqr),
+                        mapOption(pieceToEmoji),
+                        getOrElse(() => '·')
+                    )
+                ).join(' ')
+        )
+    )
+    .map((lineStr, index) => (index === 0 ? '  ' : `${8 - index + 1} `) + lineStr)
+    .join('\n')
--- src/domain/entities/board/transitions.ts
+++ src/domain/entities/board/transitions.ts
@@ -2,20 +2,18 @@ import { Board } from "./Board";
 import { EnPassant, Move, Promotion, RegularMove } from "../move/Move";
 import {omit} from 'ramda';
 import {toString} from '../square/getters';
-import { flow, pipe } from "fp-ts/lib/function";
+import { pipe } from "fp-ts/lib/function";
 import { getMoveFrom, getMoveTo, isPromotionMove, getPromotionPieceType, isRegularMove, isEnPassant, getEnPassantTakeSquare } from "../move/getters";
 import * as Eth from "fp-ts/lib/Either";
-import {getOrElse, map as mapOption} from 'fp-ts/lib/Option';
 import { getPieceAt, hasPieceAt } from "./getters";
-import { A, B, C, D, E, F, G, H, Square, _1, _2, _3, _4, _5, _6, _7, _8 } from "../square/Square";
+import { Square, _1, _2, _3, _4, _5, _6, _7, _8 } from "../square/Square";
 import { Piece } from "../piece/Piece";
 import { createPiece } from "../piece/constructors";
 import { getPieceColor } from "../piece/getters";
 import { MoveHistory } from "../movehistory/MoveHistory";
-import { createSquare } from "../square/constructors";
-import { pieceToEmoji } from "../piece/transition";
 import { moveToString } from "../move/transition";
 import { assert } from "../../../lib/either";
+import { boardToString } from "./conversions";
 
 export const removePiece = (board: Board, square:Square): Board => 
     omit([toString(square)], board);
@@ -109,102 +107,3 @@ export const applyMoveHistory = (board: Board, moveList: MoveHistory):Eth.Either
        chainApplyMove(move)
    ), Eth.right<Error,Board>(board));
 
-
-const RENDER_ORDER: Square[][] = [
-    [
-        createSquare(A, _8),
-        createSquare(B, _8),
-        createSquare(C, _8),
-        createSquare(D, _8),
-        createSquare(E, _8),
-        createSquare(F, _8),
-        createSquare(G, _8),
-        createSquare(H, _8),
-    ],
-    [
-        createSquare(A, _7),
-        createSquare(B, _7),
-        createSquare(C, _7),
-        createSquare(D, _7),
-        createSquare(E, _7),
-        createSquare(F, _7),
-        createSquare(G, _7),
-        createSquare(H, _7),
-    ],
-    [
-        createSquare(A, _6),
-        createSquare(B, _6),
-        createSquare(C, _6),
-        createSquare(D, _6),
-        createSquare(E, _6),
-        createSquare(F, _6),
-        createSquare(G, _6),
-        createSquare(H, _6),
-    ],
-    [
-        createSquare(A, _5),
-        createSquare(B, _5),
-        createSquare(C, _5),
-        createSquare(D, _5),
-        createSquare(E, _5),
-        createSquare(F, _5),
-        createSquare(G, _5),
-        createSquare(H, _5),
-    ],
-    [
-        createSquare(A, _4),
-        createSquare(B, _4),
-        createSquare(C, _4),
-        createSquare(D, _4),
-        createSquare(E, _4),
-        createSquare(F, _4),
-        createSquare(G, _4),
-        createSquare(H, _4),
-    ],
-    [
-        createSquare(A, _3),
-        createSquare(B, _3),
-        createSquare(C, _3),
-        createSquare(D, _3),
-        createSquare(E, _3),
-        createSquare(F, _3),
-        createSquare(G, _3),
-        createSquare(H, _3),
-    ],
-    [
-        createSquare(A, _2),
-        createSquare(B, _2),
-        createSquare(C, _2),
-        createSquare(D, _2),
-        createSquare(E, _2),
-        createSquare(F, _2),
-        createSquare(G, _2),
-        createSquare(H, _2),
-    ],
-    [
-        createSquare(A, _1),
-        createSquare(B, _1),
-        createSquare(C, _1),
-        createSquare(D, _1),
-        createSquare(E, _1),
-        createSquare(F, _1),
-        createSquare(G, _1),
-        createSquare(H, _1),
-    ]
-]
-
-export const boardToString  = (board:Board):string => 
-    ['A B C D E F G H'].concat(
-        RENDER_ORDER.map(
-            line => 
-                line.map(
-                    flow(
-                        sqr => getPieceAt(board, sqr),
-                        mapOption(pieceToEmoji),
-                        getOrElse(() => '·')
-                    )
-                ).join(' ')
-        )
-    )
-    .map((lineStr, index) => (index === 0 ? '  ' : `${8 - index + 1} `) + lineStr)
-    .join('\n')

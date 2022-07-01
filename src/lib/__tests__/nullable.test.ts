e3311cc51c5e32b1e82395e9cdaf5a5c3d00384c
Ali Baykal <alib@dasburo.com>
2022-07-01 20:26:01 +0200

    Introduce flowUntilNull

--- src/lib/__tests__/nullable.test.ts
+++ src/lib/__tests__/nullable.test.ts
@@ -1,4 +1,4 @@
-import { pipeUntilNull } from "../nullable";
+import { flowUntilNull, pipeUntilNull } from "../nullable";
 
 describe('lib/nullable', () => {
     describe('pipeUntilNull', () => {
@@ -43,5 +43,50 @@ describe('lib/nullable', () => {
             )
             expect(result).toBe(undefined);
         })
+    });
+
+    describe('flowUntilNull', () => {
+        it('pipes a single doubling function', () => {
+            const result = flowUntilNull<[number], number, number>(
+                n => n * 2
+            )(2);
+
+            expect(result).toBe(4);
+        });
+
+        it('pipes a single square function', () => {
+            const result = flowUntilNull<[number], number, number>(
+                n => n * n
+            )(3);
+
+            expect(result).toBe(9);
+        });
+
+        it('pipes with  multiple start values', () => {
+            const result = flowUntilNull<[number, number], number, number>(
+                (n, m) => n * m
+            )(3, 2);
+
+            expect(result).toBe(6);
+        });
+
+        it('pipes two functions', () => {
+            const result = flowUntilNull<[number], number, number, number>(
+                n => n + 2,
+                n => n * n
+            )(3);
+
+            expect(result).toBe(25);
+        });
+
+        it('returns undefined when a step returns null', () => {
+            const result = flowUntilNull<[number], number, number, number>(
+                n => n + 2,
+                () => null,
+                n => n * n
+            )(3);
+
+            expect(result).toBe(undefined);
+        });
     });
 });
--- src/lib/nullable.ts
+++ src/lib/nullable.ts
@@ -1,10 +1,11 @@
 export type Nullable<N> = N | undefined | null;
 
-export const isNull = <N>(n: Nullable<N>) => n === null || typeof n === 'undefined';
+export const isNull = <N>(n: Nullable<N>): n is (undefined | null) => n === null || typeof n === 'undefined';
+export const isNotNull = <N>(n: Nullable<N>): n is N =>  n !== null && typeof n !== 'undefined';
 
 type UnaryFn<Arg, Result> = (a: Arg) => Nullable<Result>
 
-type FuntionArgs<In, Res,  A, B, C, D, E, F, G, H> = 
+type PipeFunctionArgs<In, Res,  A, B, C, D, E, F, G, H> = 
     | [
         UnaryFn<In, Res>
     ]
@@ -79,21 +80,115 @@ export const pipeUntilNull = <
     E = unknown, 
     F = unknown, 
     G = unknown, 
-    H = unknown> (input: Input, ...fns: FuntionArgs<Input, Res, A, B, C, D, E, F, G, H>): Nullable<Res> => {
+    H = unknown> (input: Input, ...fns: PipeFunctionArgs<Input, Res, A, B, C, D, E, F, G, H>): Nullable<Res> => {
         let current = input;
 
         for(const fn of fns) {
             // @ts-expect-error
-            const interrim = fn(current);
+            current = fn(current);
 
-            if(isNull(interrim)){
+            if(isNull(current)){
                 return undefined;
             } else {
-                // @ts-expect-error
-                current = interrim;
                 continue;
             }
         }
 
         return current as unknown as Nullable<Res>;
+    }
+
+
+type FlowInitialFn <In extends unknown[], Res> = (...input:In) => Res;  
+type FlowFunctionArgs<In extends unknown[], Res,  A, B, C, D, E, F, G, H> = 
+    | [
+        FlowInitialFn<In, Res>
+    ]
+    | [
+        FlowInitialFn<In, A>,
+        UnaryFn<A, Res>
+    ]
+    | [
+        FlowInitialFn<In, A>,
+        UnaryFn<A, B>,
+        UnaryFn<B, Res>
+    ]
+    | [
+        FlowInitialFn<In, A>,
+        UnaryFn<A, B>,
+        UnaryFn<B, C>,
+        UnaryFn<C, Res>
+    ]
+    | [
+        FlowInitialFn<In, A>,
+        UnaryFn<A, B>,
+        UnaryFn<B, C>,
+        UnaryFn<C, D>,
+        UnaryFn<D, Res>
+    ]
+    | [
+        FlowInitialFn<In, A>,
+        UnaryFn<A, B>,
+        UnaryFn<B, C>,
+        UnaryFn<C, D>,
+        UnaryFn<D, E>,
+        UnaryFn<E, Res>
+    ]
+    | [
+        FlowInitialFn<In, A>,
+        UnaryFn<A, B>,
+        UnaryFn<B, C>,
+        UnaryFn<C, D>,
+        UnaryFn<D, E>,
+        UnaryFn<E, F>,
+        UnaryFn<F, Res>
+    ]
+    | [
+        FlowInitialFn<In, A>,
+        UnaryFn<A, B>,
+        UnaryFn<B, C>,
+        UnaryFn<C, D>,
+        UnaryFn<D, E>,
+        UnaryFn<E, F>,
+        UnaryFn<F, G>,
+        UnaryFn<G, Res>
+    ]
+    | [
+        FlowInitialFn<In, A>,
+        UnaryFn<A, B>,
+        UnaryFn<B, C>,
+        UnaryFn<C, D>,
+        UnaryFn<D, E>,
+        UnaryFn<E, F>,
+        UnaryFn<F, G>,
+        UnaryFn<G, H>,
+        UnaryFn<H, Res>
+    ]
+
+    export const flowUntilNull = <
+    Input extends unknown[], 
+    Res, 
+    A = unknown, 
+    B = unknown, 
+    C = unknown, 
+    D = unknown, 
+    E = unknown, 
+    F = unknown, 
+    G = unknown, 
+    H = unknown> (...fns: FlowFunctionArgs<Input, Res, A, B, C, D, E, F, G, H>) => (...input: Input):Nullable<Res> => {
+        const calcInitialFn = fns[0];
+        let current = calcInitialFn(...input);
+        const remainingFns = fns.slice(1); 
+
+        for(const fn of remainingFns) {
+            // @ts-expect-error
+            current = fn(current);
+
+            if(isNull(current)){
+                return undefined;
+            } else {
+                continue;
+            }
+        }
+
+        return current as Res;
     }
